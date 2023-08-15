d7b97e7bbf6a57c6a4bebb5275b318a21c408e37
Tanjil Ahamed <tanjil113355@gmail.com>
2023-08-15 16:01:23 +0600

    webpage compelete

--- script-2.js
+++ script-2.js
@@ -16,6 +16,11 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const userBalanceSting = userBalanceElement.innerText;
     const userBalance = parseFloat(userBalanceSting);
 
+    if (isNaN(userDepositInput)) {
+        alert('please write a number')
+        return;
+    }
+
     //deposit sum method
     const presentDeposit = userDepositInput + userPreviousDeposit;
     previousDepositElement.innerHTML = presentDeposit;
@@ -25,4 +30,40 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     userBalanceElement.innerText = presentBalance;
 });
 
-//
+//withdraw method
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    //withdraw input
+    const withdrawInput = document.getElementById('withdraw-input');
+    const userWithdrawInputString = withdrawInput.value;
+    const userWithdrawInput = parseFloat(userWithdrawInputString);
+    withdrawInput.value = '';
+
+
+    //previous deposits
+    const previousWithdrawElement = document.getElementById('withdraw');
+    const previousWithdraw = previousWithdrawElement.innerText;
+    const userPreviousWithdraw = parseFloat(previousWithdraw);
+
+    //balance
+    const userBalanceElement = document.getElementById('balance');
+    const userBalanceSting = userBalanceElement.innerText;
+    const userBalance = parseFloat(userBalanceSting);
+
+    if (userWithdrawInput > userBalance) {
+        alert('tor kase atu taka nai  ')
+        return;
+    }
+    else if (isNaN(userWithdrawInput)) {
+        alert('please write a number')
+        return;
+    }
+
+    //deposit sum method
+    const presentWithdraw = userWithdrawInput + userPreviousWithdraw;
+    previousWithdrawElement.innerHTML = presentWithdraw;
+
+    //balance sum method
+    const presentBalance = userBalance - userWithdrawInput;
+    userBalanceElement.innerText = presentBalance;
+
+})
