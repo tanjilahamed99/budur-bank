6bff2c4ae48e3eabad9fe03d610fb9987eb29732
Tanjil Ahamed <tanjil113355@gmail.com>
2023-08-15 13:09:34 +0600

    added something

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hi')
+//event
+document.getElementById('login-btn').addEventListener('click', function () {
+    //user email
+    const userEmailElement = document.getElementById('input-email');
+    const userEmail = userEmailElement.value;
+    //user password
+    const userPasswordElement = document.getElementById('input-pass');
+    const userPassword = userPasswordElement.value;
+
+    //login into the bank
+    if (userEmail === 'budu@gmail.com' && userPassword === '12345') {
+        window.location.href='index-2.html';
+    } else (
+        alert('toi password bhu le gesos !! tore tejjo koyra dilam')
+    )
+
+});
